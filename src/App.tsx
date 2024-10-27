9ccb5962f1e35a780f77d89c75d16c683df79ecd
kesalkarsaket <saketkesalkar98@gmail.com>
2024-10-27 11:07:43 +0530

    crypto-hunter-updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/HomePage";
+import CoinPage from "./pages/CoinPage";
+import Alert from "./components/Alert";
 
-function App() {
+const useStyles = makeStyles({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+});
+
+function App(): JSX.Element {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+        <Alert />
+      </BrowserRouter>
     </div>
   );
 }
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA4YvxWI4fZPGpsq7X4hOPMdGU_SWsPXPk",
-  authDomain: "crypto-app-e340b.firebaseapp.com",
-  projectId: "crypto-app-e340b",
-  storageBucket: "crypto-app-e340b.appspot.com",
-  messagingSenderId: "30397204197",
-  appId: "1:30397204197:web:5967372fcff7c5d26cf60c",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 export default firebaseConfig;
